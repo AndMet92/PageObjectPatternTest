b7704c022b2c6b8277e012f5fae42d5d7cf9e2ad
AndMet92 <83501512+AndMet92@users.noreply.github.com>
2021-05-05 15:34:28 +0300

    Update POC.java

--- src/test/java/selenide/POC.java
+++ src/test/java/selenide/POC.java
@@ -10,9 +10,13 @@ public class POC {
     public void selenide() {
         // Цель: проверить соответствует ли введенный текст в поле задачи, сохраненному.
         open("http://todomvc4tasj.herokuapp.com");
-        element("#new-todo").setValue("c").pressEnter();
-        elements(" #todo-list > li > div > label > font > font ").shouldHave(exactTexts("c"));
+        element("#new-todo").setValue("a").pressEnter();
+        elements(" #todo-list > li > div > label > font > font ").shouldHave(exactTexts("a"));
         
+        
+        // elements("#todo-list").shouldHave(CollectionCondition.texts("a"));
+
+      //  elements("#todo-list li label font").shouldHave(CollectionCondition.texts("a"));
 
 
 
